d329393f54c2f3ece956d9890c33e184a4209e93
brauejm <brauer_matt@cat.com>
2021-01-28 14:46:18 -0700

    create _group_files_by_year

--- repo/tabular_files.py
+++ repo/tabular_files.py
@@ -21,3 +21,33 @@ def import_call_schedule_data(directory):
     data_file_names = [f for f in listdir(directory) if f[0:21] == "FFIEC CDR Call Subset"]
 
     return data_file_names
+
+def _group_files_by_year(file_list):
+    """
+    Groups call report files in list by year in file name
+
+        Parameters
+        ----------
+        file_list: list
+            list of call data file names
+
+        Returns
+        -------
+        grouped_file_list: list
+            list of call data file names grouped by year
+
+        Raises
+        ------
+    """
+    grouped_file_list = []
+    previous_year = ""
+    subgroup = []
+    for file_name in file_list:
+        year = file_name.split(" ")[-1:].split("(")
+        if year != previous_year:
+            grouped_file_list.append(subgroup)
+            subgroup = []
+
+        subgroup.append(file_name)
+
+    return grouped_file_list
