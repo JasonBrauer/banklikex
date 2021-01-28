8d012a9824db2a286f6efce0cd5f079b0e18d530
brauejm <brauer_matt@cat.com>
2021-01-28 13:16:39 -0700

    start defining import call schedule data

---
+++ repo/tabular_files.py
@@ -0,0 +1,20 @@
+import csv
+
+def import_call_schedule_data(directory):
+    """
+    Imports data from tab delimited call reports
+
+        Parameters
+        ----------
+        directory: str
+            directory containing call schedule tab delimited files
+
+        Returns
+        -------
+        call_object_list: list
+            list of call data objects filled with imported data
+
+        Raises
+        ------
+    """
+    pass
