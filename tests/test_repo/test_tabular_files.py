3679202d03a3c5c99628c7377d734e938a5d68ba
brauejm <brauer_matt@cat.com>
2021-01-28 14:16:02 -0700

    test grouped files by year success

---
+++ tests/test_repo/test_tabular_files.py
@@ -0,0 +1,39 @@
+import unittest
+
+class TestGroupedFilesByYear(unittest.TestCase):
+    """
+    Tests _grouped_files_by_year function
+    """
+
+    '''
+        TODO - write error handling tests
+    '''
+
+    def test_grouped_files_by_year_success(self):
+        """
+        Test the successful use of grouped files by year
+        """
+
+        from repo.tabular_files import _grouped_files_by_year
+
+        test_file_list = [
+            "FFIEC CDR Call Subset of Schedules 2020(1 of 2)",
+            "FFIEC CDR Call Subset of Schedules 2020(2 of 2)",
+            "FFIEC CDR Call Subset of Schedules 2019(1 of 2)",
+            "FFIEC CDR Call Subset of Schedules 2019(2 of 2)",
+        ]
+
+        expected_output = [
+            [
+                "FFIEC CDR Call Subset of Schedules 2020(1 of 2)",
+                "FFIEC CDR Call Subset of Schedules 2020(2 of 2)"
+            ],
+            [
+                "FFIEC CDR Call Subset of Schedules 2019(1 of 2)",
+                "FFIEC CDR Call Subset of Schedules 2019(2 of 2)"
+            ]
+        ]
+
+        grouped_output = _grouped_files_by_year(test_file_list)
+
+        self.assert_equal(grouped_output, expected_output)
