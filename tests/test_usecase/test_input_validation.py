f3a8d0c587c7e3b81801e709f0b5cc218ba73501
Jason Brauer <jbrauer90@gmail.com>
2021-01-29 16:19:31 -0700

    validate directory and test

--- tests/test_usecase/test_input_validation.py
+++ tests/test_usecase/test_input_validation.py
@@ -1,4 +1,6 @@
 import unittest
+from unittest.mock import patch
+
 from entities.data_models import CallData
 
 class TestValidateCallDataObjectList(unittest.TestCase):
@@ -25,3 +27,22 @@ class TestValidateCallDataObjectList(unittest.TestCase):
             validate_call_data_object_list,
             bad_call_data_object_list
         )
+
+class TestValidateDirectory(unittest.TestCase):
+    """
+    Tests _grouped_files_by_year function
+    """
+
+    @patch("usecase.input_validation.os.path.isdir")
+    def test_valid_directory(self, mock_isdir):
+        from usecase.input_validation import validate_directory
+
+        mock_isdir.return_value = True
+        test_dir = ".\\some\\directory"
+
+        result = validate_directory(test_dir)
+
+        self.assertEqual(test_dir, result)
+        mock_isdir.assert_called_once_with(test_dir)
+
+
--- usecase/input_validation.py
+++ usecase/input_validation.py
@@ -1,4 +1,5 @@
 from entities.data_models import CallData
+import os
 
 def validate_call_data_object_list(call_data_object_list):
     """
@@ -21,3 +22,24 @@ def validate_call_data_object_list(call_data_object_list):
         assert type(obj) == CallData, "each item in call_data_object_list must be a CallData object"
     
     return call_data_object_list
+
+
+def validate_directory(directory):
+    """
+    Validates that a directory is available
+        Parameters
+        ----------
+        directory: str
+            path to a directory
+
+        Returns
+        -------
+        directory: list
+            validated path to a directory
+
+        Raises
+        ------
+    """
+    assert os.path.isdir(directory), "not a valid or available directory"
+
+    return directory
