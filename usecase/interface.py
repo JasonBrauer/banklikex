c444ef7d994b3df7cd113032b66c81a5ec316fa2
Jason Brauer <jbrauer90@gmail.com>
2021-01-29 16:29:14 -0700

    start load_call_data

---
+++ usecase/interface.py
@@ -0,0 +1,20 @@
+from repo.tabular_files import import_call_schedule_data
+
+def load_call_data(valid_directory):
+    """
+    Retrieves a list of call data objects
+        Parameters
+        ----------
+        valid_directory: str
+            a validated directory
+
+        Returns
+        -------
+        call_data_object_list: list
+            list of call data objects
+
+        Raises
+        ------
+    """
+    pass
+
