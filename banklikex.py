08a1ca6a16c0874126dd07208878d340d018056d
Jason Brauer <jbrauer90@gmail.com>
2021-01-31 12:37:40 -0700

    formatting cli help

--- banklikex.py
+++ banklikex.py
@@ -4,13 +4,15 @@ from usecase.interface import load_call_data, find_similar_banks, idrssd_to_bank
 from usecase.input_validation import (validate_idrssd, validate_directory, 
 validate_call_data_object_list)
 
-parser = argparse.ArgumentParser(description='Find banks similar to provided bank.')
+parser = argparse.ArgumentParser(description='Find banks similar to provided bank.',
+    formatter_class=argparse.RawTextHelpFormatter)
 
 parser.add_argument("--find_similar_banks", nargs=2,
-help="finds banks similar to the one identified via bank idrssd\
-    arg_1: bank_idrssd- idrssd of bank that will be used to find similar banks\
-    arg_2: directory- directory where bank call data is stored")
-parser.add_argument("--show_bank_names", action="store_true")
+    help="finds banks similar to the one identified via bank idrssd\
+    \narg_1: bank_idrssd - idrssd of bank that will be used to find similar banks\
+    \narg_2: directory - directory where bank call data is stored")
+parser.add_argument("--show_bank_names", action="store_true", 
+    help="used with find similar banks to also list bank names")
 # parser.add_argument("--plot", help="plots the distributions of all fields used to compare banks")
 
 args = parser.parse_args()
