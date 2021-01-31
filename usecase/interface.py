c11c64db9435d0fd25942abed1bfb545a56ef444
Jason Brauer <jbrauer90@gmail.com>
2021-01-30 21:47:46 -0700

    finish find similar banks

--- usecase/interface.py
+++ usecase/interface.py
@@ -1,5 +1,8 @@
 from repo.tabular_files import import_call_schedule_data
 
+from usecase.analysis import (find_all_periods, average_all_periods, create_data_dict_list, 
+create_distribution, find_similar_bank_field, find_similar_bank_intersection)
+
 def load_call_data(valid_directory):
     """
     Retrieves a list of call data objects
@@ -50,6 +53,45 @@ def find_distinct_idrssd(valid_call_data_object_list):
         distinct_idrssd_list.append(data_obj.idrssd)
 
     return distinct_idrssd_list
+
+def find_similar_banks(valid_idrssd, valid_call_data_object_list):
+    """
+    Finds a list of banks similar to the one provided via idrssd 
+        Parameters
+        ----------
+        valid_idrssd: int
+            validated bank unique idrssd
+        valid_call_data_object_list: list
+            validated list of call data objects
+
+        Returns
+        -------
+        matching_agg_obj_list: AggregateCallData object
+            list of aggregate call data objects for banks similar to provided bank
+
+        Raises
+        ------
+    """
+    unique_idrssd_list = find_distinct_idrssd(valid_call_data_object_list)
+
+    period_agg_obj_list = []
+    for idrssd in unique_idrssd_list:
+        grouped_by_idrssd = find_all_periods(valid_call_data_object_list, idrssd)
+        average_call_data = average_all_periods(grouped_by_idrssd)
+        period_agg_obj_list.append(average_call_data)
+
+    data_dict_list = create_data_dict_list(period_agg_obj_list)
+
+    for field in data_dict_list:
+        ecdf = create_distribution(data_dict_list, field)
+        find_similar_bank_field(valid_idrssd, period_agg_obj_list, ecdf, field)
+
+    matching_agg_obj_list = find_similar_bank_intersection(period_agg_obj_list)
+
+    return matching_agg_obj_list
+
+    
+
 
 
 
